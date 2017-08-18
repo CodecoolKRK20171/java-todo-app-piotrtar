4c9262b8d86d857af6e31211739492db2a11fff2
piotrtar <piotr.tarczon@gmail.com>
2017-08-18 13:45:59 +0200

    Create TodoItem class

--- TodoItem.java
+++ TodoItem.java
@@ -1,7 +1,22 @@
+import java.util.ArrayList;
 /**
  * Class representing a single todo item.
  * 
  */
 public class TodoItem{
+    private String name;
+    Boolean is_completed = false;
+
+    public TodoItem(String name){
+        this.name = name;
+
+        }
+    public void get_completed(TodoItem this){
+        this.is_completed = true;
+    }
+
+    public String getName(){
+        return this.name;
+    }
 
 }
